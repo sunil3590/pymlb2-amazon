f6976bf0043582ee2c704014600f1d568db0d608
Sunil D S <sunil3590@gmail.com>
2018-06-18 17:39:49 +0530

    add andsearch product

--- run.py
+++ run.py
@@ -1,26 +1,35 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory, jsonify
 
 app = Flask('Amazon')
+
+products = []
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'Welcome!'
+    return send_from_directory('static', 'index.html')
 
 
-# http://127.0.0.1:5005/say_hello?name=Sunil
-@app.route('/say_hello', methods=['GET', 'POST'])
-def say_hello():
+@app.route('/api/product', methods=['GET', 'POST'])
+def product():
     if request.method == 'GET':
-        return '[GET] Hello, ' + request.args['name']
+        query = request.args['name']
+        for prod in products:
+            if prod['name'] == query:
+                return jsonify(prod)
+        return 'No match'
     elif request.method == 'POST':
-        return '[POST] Hello, ' + request.form['name'] + ' Age ' + request.form['age']
-
-
-@app.route('/say_bye', methods=['GET'])
-def say_bye():
-    return 'Bye!'
+        name = request.form['name']
+        desc = request.form['desc']
+        price = request.form['price']
+        prod = {
+            'name': name,
+            'desc':  desc,
+            'price': price
+        }
+        products.append(prod)
+        return send_from_directory('static', 'index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5005)
+    app.run(host='0.0.0.0', port=5005, debug=True)
